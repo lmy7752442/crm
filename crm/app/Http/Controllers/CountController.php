5cc65ccbda77c65bd3e0c5e14805d9e1165d2a09
luo <luo@luo.com>
2018-08-29 20:27:36 +0800

    ll

--- crm/app/Http/Controllers/CountController.php
+++ crm/app/Http/Controllers/CountController.php
@@ -34,33 +34,18 @@ class CountController extends CommonController{
                 $arr[$k]['save_count'] = DB::table('record')->where('a_id',$v->a_id)->where('action','like','%修改%')->count();
             }
         }else{
-            foreach($admin_data as $k=>$v){
-                $arr[$k]['admin_name'] = $v->a_account;
-                $arr[$k]['user_count'] = DB::table('customer')->where('a_id',$v->a_id)->where('status',1)->count();
-                $arr[$k]['documentary_count'] = DB::table('documentary')->where('status',1)->where('admin_id',$v->a_id)->count();
-                $order_data = DB::table('order')->where('status',1)->where('a_id',$v->a_id)->get();
-                $arr[$k]['order_count'] = count($order_data);
+                $arr[1]['admin_name'] = $admin_data->a_account;
+                $arr[1]['user_count'] = DB::table('customer')->where('a_id',$admin_data->a_id)->where('status',1)->count();
+                $arr[1]['documentary_count'] = DB::table('documentary')->where('status',1)->where('admin_id',$admin_data->a_id)->count();
+                $order_data = DB::table('order')->where('status',1)->where('a_id',$admin_data->a_id)->get();
+                $arr[1]['order_count'] = count($order_data);
                 $sales = 0;
                 foreach($order_data as $v){
                     $sales += $v->get_money;
                 }
-                $arr[$k]['sale'] = $sales;
-
-                $arr[$k]['del_count'] = DB::table('record')->where('a_id',$v->a_id)->where('action','like','%删除%')->count();
-                $arr[$k]['save_count'] = DB::table('record')->where('a_id',$v->a_id)->where('action','like','%修改%')->count();
-            }
-//            $admin_name[] = $admin_data->a_account;
-//            $user_count[] = DB::table('customer')->where('a_id',$a_id)->where('status',1)->count();
-//            $documentary_count[] = DB::table('documentary')->where('status',1)->where('admin_id',$a_id)->count();
-//            $order_data = DB::table('order')->where('status',1)->where('a_id',$a_id)->get();
-//            $order_count[] = count($order_data);
-//            $sales = 0;
-//            foreach($order_data as $v){
-//                $sales += $v->get_money;
-//            }
-//            $sale[] = $sales;
-//            $del_count[] = DB::table('record')->where('a_id',$a_id)->where('action','like','%删除%')->count();
-//            $save_count[] = DB::table('record')->where('a_id',$a_id)->where('action','like','%修改%')->count();
+                $arr[1]['sale'] = $sales;
+                $arr[1]['del_count'] = DB::table('record')->where('a_id',$admin_data->a_id)->where('action','like','%删除%')->count();
+                $arr[1]['save_count'] = DB::table('record')->where('a_id',$admin_data->a_id)->where('action','like','%修改%')->count();
         }
         return view('count.count_list',['data'=>$arr]);
     }
